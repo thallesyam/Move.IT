d51c89a90b3871471a4236220a293951e9726226
Thalles Ian <thallesyam@users.=.github.com>
2021-02-22 21:46:16 -0300

    Creating ExperienceBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React from "react";
+import "./styles/global.css";
+import ExperienceBar from "./components/ExperienceBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
